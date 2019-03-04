d2a8f4483a3505674a3b84229a80246af35ff544
brombaut <ben.rombaut@unb.ca>
2019-03-04 13:11:39 -0400

    Finished login page and logic stuff

--- app.js
+++ app.js
@@ -17,29 +17,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get('/', function(request, response) {
   connection.query('SELECT * FROM users', function(err, rows){
-    res.render('index', {users : rows});
+    response.render('index', {users : rows});
   });
 });
 
-app.get('/login', function(req, res) {
-  res.render('login');
+app.get('/login', function(request, response) {
+  response.render('login');
 });
 
-app.post('/login', urlencodedParser, function(req, res) {
-  // Prepare output in JSON format
-  response = {
-    first_name:req.body.username,
-    last_name:req.body.password
-  };
-  console.log(response);
-  // res.end(JSON.stringify(response));
+app.post('/login', function(request, response) {
+  const usernameIn = request.body.username;
+  const passwordIn = request.body.password;
+  connection.query('SELECT * FROM users WHERE username = "' + usernameIn + '"', function(err, rows){
+    if (rows.length === 0) {
+      response.status(404).send('User not found');
+      return;
+    }
+    const userObj = rows[0];
+    if (passwordIn !== userObj.password) {
+      response.status(403).send('Invalid Credentials');
+      return;
+    }
+    response.send(userObj);
+  });
 });
 
-// what port to run server on
 app.listen(3000, function () {
   console.log('Server started on port 3000');
 });

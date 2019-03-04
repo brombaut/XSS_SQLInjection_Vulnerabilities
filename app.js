c5cbf9181939cea4839ab6a0ed615ee8331ce8a2
brombaut <ben.rombaut@unb.ca>
2019-03-04 10:49:01 -0400

    Cleaning up db schema;

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 var app = express();
 
 var connection = mysql.createConnection({
@@ -16,10 +17,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', function(req, res) {
   connection.query('SELECT * FROM users', function(err, rows){
     res.render('index', {users : rows});
   });
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.post('/login', urlencodedParser, function(req, res) {
+  // Prepare output in JSON format
+  response = {
+    first_name:req.body.username,
+    last_name:req.body.password
+  };
+  console.log(response);
+  // res.end(JSON.stringify(response));
 });
 
 // what port to run server on
--- db/forum_init.sql
+++ db/forum_init.sql
@@ -2,17 +2,27 @@ DROP DATABASE IF EXISTS cs4417_forum_database;
 CREATE DATABASE cs4417_forum_database;
 USE cs4417_forum_database;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, posts;
 
 CREATE TABLE users (
-    username              VARCHAR(64)     NOT NULL,
-    password              VARCHAR(64)     NOT NULL,
-    first_name            VARCHAR(1024)     NOT NULL,
-    last_name             VARCHAR(1024)     NOT NULL,
-    UNIQUE (username),
-    PRIMARY KEY (username)
+  id                    INT             NOT NULL AUTO_INCREMENT,
+  username              VARCHAR(64)     NOT NULL,
+  password              VARCHAR(64)     NOT NULL,
+  first_name            VARCHAR(1024)   NOT NULL,
+  last_name             VARCHAR(1024)   NOT NULL,
+  UNIQUE (username),
+  PRIMARY KEY (id)
 );
 
-INSERT INTO users (username, password, first_name, last_name) VALUES ('test1', 'test1', 'test1', 'test1');
-INSERT INTO users (username, password, first_name, last_name) VALUES ('test2', 'test2', 'test2', 'test2');
-INSERT INTO users (username, password, first_name, last_name) VALUES ('test3', 'test3', '<script>alert("hi")</script>', 'test3');
+CREATE TABLE posts (
+  id                    INT             NOT NULL AUTO_INCREMENT,
+  content               VARCHAR(1024)   NOT NULL,
+  createdAt             DATETIME        NOT NULL,
+  createdBy             INT             NOT NULL,
+  FOREIGN KEY (createdBy) REFERENCES users (id),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO users (username, password, first_name, last_name) VALUES ('brombaut', 'testing123', 'Ben', 'Rombaut');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('jdoe', 'testing123', 'John', 'Doe');
+-- INSERT INTO users (username, password, first_name, last_name) VALUES ('test3', 'test3', '<script>alert("hi")</script>', 'test3');

